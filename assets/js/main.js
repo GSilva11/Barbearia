9b37275d7c44b6698b460b5ec7f14f48d8426691
Gabriel Silva <gabrielsilvagv01@gmail.com>
2024-11-11 22:38:34 -0300

    menu hamburguer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,15 @@
-
 const hamburguer = document.querySelector(".hamburguer");
 
 const nav = document.querySelector(".navbar");
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
 
-
-// const links = document.querySelectorAll('#navbar-items a');
-// links.addEventListener('click', () => {
-//             nav.classList.toggle('active');
-// });
-
-// Smooth Scrolling
-$("#navbar a, .btn").on("click", function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
-
-        const hash = this.hash;
-
-        $("html, body").animate(
-            {
-                scrollTop: $(hash).offset().top - 60
-            },
-            800
-        );
-    }
-});
+function addListener() {
+	const nav = document.querySelector('nav')
+    const listItems = document.querySelectorAll('#navbar-items > li');
+	console.log('list', listItems)
+	listItems.forEach(li => {
+		const anchor = li.querySelector('a')
+		anchor.addEventListener('click', () => nav.classList.remove('active'))
+	})
+}
+addListener()
